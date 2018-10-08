34ae88dfb3cfa3e019c676860dfd72ba6899e604
david <david@davidberry.io>
2018-10-07 20:44:30 -0700

    offline status listener

--- src/message-board.js
+++ src/message-board.js
@@ -1,5 +1,12 @@
 import { createStore } from 'redux';
 
+export const ONLINE = `ONLINE`;
+export const AWAY = `AWAY`;
+export const BUSY = `BUSY`;
+export const OFFLINE = `OFFLINE`;
+
+export const UPDATE_STATUS = `UPDATE_STATUS`;
+
 const defaultState = {
 	messages: [{
 		date: new Date('2016-10-10 10:11:55'),
@@ -14,12 +21,20 @@ const defaultState = {
 		postedBy: `Llewlyn`,
 		content: `Anyone got tickets to ng-conf?`
 	}],
-	userStatus: `ONLINE`
+	userStatus: ONLINE
 };
 
-const store = createStore((state = defaultState) => {
+const reducer = (state = defaultState, {type, value}) => {
+	switch (type) {
+		case UPDATE_STATUS:
+			return {...state, userStatus: value};
+			break;
+	}
+
 	return state;
-});
+}
+
+const store = createStore(reducer);
 
 const render = () => {
 	const { messages, userStatus } = store.getState();
@@ -31,6 +46,21 @@ const render = () => {
 			</div>
 		`))
 		.join("");
+
+	document.forms.newMessage.fields.disabled = (userStatus === OFFLINE);
 };
 
+const statusUpdateAction = (value) => {
+	return {
+		type: UPDATE_STATUS,
+		value
+	}
+};
+
+document.forms.selectStatus.status.addEventListener("change", (e) => {
+	store.dispatch(statusUpdateAction(e.target.value));
+});
+
 render();
+
+store.subscribe(render);
